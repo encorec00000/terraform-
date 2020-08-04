48601dfb0e988849874e94a15887408567d01a01
Albert Tseng <albert@AlbertdeMacBook-Pro.local>
2020-08-04 16:46:54 +0800

    update

--- modules/IAM/outputs.tf
+++ modules/IAM/outputs.tf
@@ -6,6 +6,9 @@ output "created_users" {
   value = values(aws_iam_user._users)[*]
 }
 
+output "created_access_keys" {
+  value = values(aws_iam_user._access_key)[*]
+}
 
 output "created_policies" {
   value = values(aws_iam_policy._policies)[*]
